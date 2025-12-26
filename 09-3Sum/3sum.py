2ac97a58161ee679518f8229821641eb66b5692e
ariana0505 <penaariana075@gmail.com>
2025-12-26 00:24:20 -0500

    recordando y mejorando error

--- 09-3Sum/3sum.py
+++ 09-3Sum/3sum.py
@@ -1,19 +1,25 @@
 nums = [-1,0,1,2,-1,-4]
-answer = []
 nums.sort()
-for i, a in enumerate(nums):
-    if i> 0 and a == nums[i-1]:
+answer = []
+for i , v in enumerate(nums):
+    if i> 0 and nums[i] == nums[i-1]:
         continue
-    l, r = i+ 1, len(nums)-1
-    while l< r:
-        suma3 = a + nums[l] + nums[r]
-        if 0 < suma3:
-            r = r - 1 
-        elif 0> suma3:
-            l = l +1 
-        else:
-            answer.append([a, nums[l], nums[r] ])
+    l , r = i +1 , len(nums) - 1
+    while l < r :
+        sum3 = v + nums[l] + nums[r]
+        if 0 > sum3:
             l += 1
-            while nums[l] == nums[l-1] and l<r:
-                l+=1
+        elif 0 < sum3:
+            r -= 1
+        else:
+            answer.append([v , nums[l] , nums[r]])
+            l += 1
+            r -= 1
+
+            while l < r and nums[l] == nums[l - 1]:
+                l += 1
+
+            while l < r and nums[r] == nums[r + 1]:
+                r -= 1
+
 print(answer)
