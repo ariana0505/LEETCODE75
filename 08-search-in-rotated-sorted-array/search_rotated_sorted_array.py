5218864d0a3cda59bd030260f77e6c1a1ee535c4
ariana0505 <penaariana075@gmail.com>
2025-12-25 23:30:08 -0500

    recordando

--- 08-search-in-rotated-sorted-array/search_rotated_sorted_array.py
+++ 08-search-in-rotated-sorted-array/search_rotated_sorted_array.py
@@ -1,20 +1,21 @@
 nums = [4,5,6,7,0,1,2]
-target = 8
-l, r = 0, len(nums) - 1
+target = 9
+l = 0
+r = len(nums) - 1
 while l <= r:
-    mid =  (l + r) // 2
-    if target == nums[mid]:
-        print(mid)
+    m = (l + r) //2
+    if nums[m] == target:
+        print(m)
         break
-    if nums[l] <= nums[mid]:
-        if nums[l] <= target < nums[mid]:
-            r = mid - 1
+    if nums[l] < nums[m]:
+        if nums[l] <= target < nums[m]:
+            r = m - 1
         else:
-            l = mid + 1
+            l = m + 1 
     else:
-        if nums[mid] < target <= nums[r]:
-            l = mid + 1
+        if nums[m] < target <= nums[r]:
+            l = m + 1
         else:
-            r = mid -1
+            r = m - 1
 else:
     print(-1)
