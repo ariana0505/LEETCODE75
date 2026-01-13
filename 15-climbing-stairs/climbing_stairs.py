39cd581a2c9962d41edbb82d7decf2dae9a558c5
ariana0505 <penaariana075@gmail.com>
2026-01-12 23:28:17 -0500

    recordadmp

--- 15-climbing-stairs/climbing_stairs.py
+++ 15-climbing-stairs/climbing_stairs.py
@@ -1,7 +1,8 @@
-n = 3 
-uno , dos = 1, 1
+n = 3
+uno = 1
+dos = 1
 for i in range(n - 1):
-    temp = uno
-    uno = uno + dos
-    dos = temp
+    temp = uno 
+    uno = dos + uno
+    dos = uno
 print(uno)
