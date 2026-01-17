80fb75a13a65a88b0099e2a6c2a1d34719919602
ariana0505 <penaariana075@gmail.com>
2026-01-16 19:05:18 -0500

    fix

--- 15-climbing-stairs/climbing_stairs.py
+++ 15-climbing-stairs/climbing_stairs.py
@@ -1,8 +1,17 @@
-n = 3
+n = 8 
 uno = 1
 dos = 1
-for i in range(n - 1):
-    temp = uno 
-    uno = dos + uno
-    dos = uno
-print(uno)
+
+# Repetimos el proceso n veces
+for i in range(n):
+    # Guardamos el valor actual de 'uno'
+    temp = uno
+
+    # Sumamos los dos números
+    uno = uno + dos
+
+    # Actualizamos 'dos' con el valor anterior
+    dos = temp
+
+# Mostramos el último valor guardado
+print(temp)
